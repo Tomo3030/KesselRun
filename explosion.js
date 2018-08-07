feac5e20d3151c310a9d3bb184fa4a8f9ed4e73d
Tim Thompson <timt333@gmail.con>
2018-08-07 21:47:00 +0900

    working fine with auto movement. Attempting to make score.

--- explosion.js
+++ explosion.js
@@ -1,19 +1,14 @@
-function explosion(x,y) {
-	let particles = []
-	const numberOfParticles = 20;
+this.makeParticles = function(x,y){
+        const numberOfParticles = 20;
+        for (var i = 0; i < numberOfParticles; i++) {
+            particleAngle = 1;
+            particles.push(new component(5,5,"white",x,y));
+            particles[i].speed = 5;
+            particles[i].angle = particleAngle;
+            console.log(particles[i].angle);
+            console.log(particles[i].speed);
 
-	for (var i = 0; i < numberOfParticles; i++) {
-		particles.push(new component(5,5,"white",x,y)
-	}
-
-	for (var i = 0; i < particles.length; i++) {
-		//make a randome angle
-		randAngle = Math.PI/180 * ((Math.random() * Math.PI) + Math.PI);
-		//assign it to the particle
-		particles[i].angle = randAngle;
-		particles[i].speed = 5;
-		particles[i].updateTurn();
-
-
-
-	}
+            explode = true;
+    
+    }
+}
